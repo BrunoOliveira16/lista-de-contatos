e479d52a3aab48de9e59ed4dc9e7ea2aff283421
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2024-06-14 16:15:22 -0300

    feat: instalado redux toolkit e configurado store e reducers

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
 import { Router } from './routes/routes'
+import store from './store'
 
 import { GlobalStyle } from './styles/globalStyles'
 
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <Router />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Router />
+      </BrowserRouter>
+    </Provider>
   )
 }
 
