af610cbe4341a2787b741a14d028070a96e48243
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2024-10-08 17:20:35 -0300

    feat: adicionado mock de contacts

--- src/store/reducers/contacts.ts
+++ src/store/reducers/contacts.ts
@@ -1,40 +1,13 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { Contact } from '../../models/Contacts'
+import { contacts } from '../../mock/index'
 
 type ContactsState = {
   items: Contact[]
 }
 
 const initialState: ContactsState = {
-  items: [
-    {
-      id: 1,
-      name: 'Batman',
-      phone: '2199999999',
-      email: 'bruce@email.com',
-      image:
-        'https://png.pngtree.com/thumb_back/fh260/background/20230518/pngtree-the-batman-face-is-standing-on-a-dark-background-image_2573605.jpg',
-      isFavorite: true
-    },
-    {
-      id: 2,
-      name: 'Homen-Aranha',
-      phone: '2199999888',
-      email: 'peter@email.com',
-      image:
-        'https://i.pinimg.com/550x/9c/ba/8b/9cba8bd121fac637e1b4b0b49d18c91e.jpg',
-      isFavorite: false
-    },
-    {
-      id: 3,
-      name: 'Superman',
-      phone: '2188899999',
-      email: 'clark@email.com',
-      image:
-        'https://rollingstone.uol.com.br/media/_versions/2024/03/james-gunn-revela-primeira-imagem-e-anuncia-novo-titulo-de-superman-legacy-foto-reproducaodc-comics_widelg.jpg',
-      isFavorite: true
-    }
-  ]
+  items: contacts
 }
 
 const contactSlice = createSlice({
@@ -65,6 +38,6 @@ const contactSlice = createSlice({
   }
 })
 
-export const { remove } = contactSlice.actions
+export const { remove, register } = contactSlice.actions
 
 export default contactSlice.reducer
