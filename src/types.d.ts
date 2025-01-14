a9b3f043926f13e57115b0f7201046e004a459ed
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2025-01-14 20:06:23 -0300

    feat: adicionado novos types e ajuste no mock

--- src/types.d.ts
+++ src/types.d.ts
@@ -6,3 +6,18 @@ declare type ContactDataProps = {
   image: string
   isFavorite: boolean
 }
+
+declare type FormFields = 'name' | 'phone' | 'image' | 'email' | 'isFavorite'
+
+declare type FormErrors = {
+  name: boolean
+  phone: boolean
+}
+
+declare type FormState = {
+  name: string
+  phone: string
+  image: string
+  email: string
+  isFavorite: boolean
+}
